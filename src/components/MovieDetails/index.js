c713952e5ee65a413a0a4191c551ff3974d30581
Hamrah Khamsehzadeh <52994357+haamraah@users.noreply.github.com>
2023-05-08 05:36:29 +0000

    Update 3 files

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,7 +1,15 @@
-function MovieDetails() {
+import { useParams } from "react-router-dom";
+
+function MovieDetails(props) {
+  let { movieId } = useParams();
+  const { movies } = props;
+  const [movie] = movies.filter((movie) => {
+    return movie.id == movieId;
+  });
   return (
-    <div className='comp purple'>
-      <h1>MovieDetails Component</h1>
+    <div className="comp purple">
+      <h1>{movie.title}</h1>
+      <p>{movie.description}</p>
     </div>
   );
 }
--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,25 @@
-function Movies() {
+import { NavLink, Route, Switch } from "react-router-dom";
+import MoviesDetails from "../MovieDetails";
+
+function Movies(props) {
+  const { movies } = props;
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Movies Component</h1>
+      <nav>
+        {movies.map((movie) => {
+          return (
+            <li key={movie.id}>
+              <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+            </li>
+          );
+        })}
+      </nav>
+      <Switch>
+        <Route path="/movies/:movieId">
+          <MoviesDetails movies={movies} />
+        </Route>
+      </Switch>
     </div>
   );
 }
